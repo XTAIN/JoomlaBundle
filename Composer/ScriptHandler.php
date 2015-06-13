3c419eae114ccdb20c4369fb0d6b9dcf9e3aca87
Maximilian Ruta <mr@xtain.net>
2015-06-13 14:32:19 +0200

    update composer

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -69,8 +69,6 @@ class ScriptHandler
             return;
         }
 
-        var_dump($options);
-        /*
         $username = '';
         if ($options['joomla-admin-username'] !== null) {
             $username = '--username='.escapeshellarg($options['joomla-admin-username']).' ';
@@ -85,8 +83,8 @@ class ScriptHandler
         if ($options['joomla-admin-password'] !== null) {
             $password = '--username='.escapeshellarg($options['joomla-admin-password']).' ';
         }
-        */
 
+        var_dump('xtain:joomla:install '.$username.' '.$email.' '.$password);
         static::executeCommand($event, $consoleDir, 'xtain:joomla:install '.$username.' '.$email.' '.$password);
     }
 
