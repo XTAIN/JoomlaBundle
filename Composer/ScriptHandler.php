7909b9780b70eed704a4a2a153fd885b2c49b436
Maximilian Ruta <mr@xtain.net>
2015-06-13 13:50:11 +0200

    add scriptdrunner

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * This file is part of the XTAIN Joomla package.
+ *
+ * (c) Maximilian Ruta <mr@xtain.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace XTAIN\Bundle\JoomlaBundle\Composer;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Composer\Script\CommandEvent;
+
+/**
+ * Class ScriptHandler
+ *
+ * @author Maximilian Ruta <mr@xtain.net>
+ * @package XTAIN\Bundle\JoomlaBundle\Composer
+ */
+class ScriptHandler
+{
+    /**
+     * Composer variables are declared static so that an event could update
+     * a composer.json and set new options, making them immediately available
+     * to forthcoming listeners.
+     */
+    protected static $options = array(
+        'symfony-web-dir' => 'web',
+        'joomla-assets-install' => 'symlink',
+        'joomla-admin-username' => null,
+        'joomla-admin-email' => null,
+        'joomla-admin-password' => null
+    );
+
+    public static function installAssets(CommandEvent $event)
+    {
+        $options = static::getOptions($event);
+        $consoleDir = static::getConsoleDir($event, 'install joomla');
+
+        if (null === $consoleDir) {
+            return;
+        }
+
+        $webDir = $options['symfony-web-dir'];
+
+        $symlink = '--mode=relative';
+        if ($options['joomla-assets-install'] == 'copy') {
+            $symlink = '--mode=copy';
+        } elseif ($options['joomla-assets-install'] == 'symlink') {
+            $symlink = '--mode=symlink ';
+        }
+
+        if (!static::hasDirectory($event, 'symfony-web-dir', $webDir, 'install joomla')) {
+            return;
+        }
+
+        static::executeCommand($event, $consoleDir, 'xtain:joomla:assets:install '.$symlink.escapeshellarg($webDir));
+    }
+
+    /**
+     * Returns a relative path to the directory that contains the `console` command.
+     *
+     * @param CommandEvent $event      The command event.
+     * @param string       $actionName The name of the action
+     *
+     * @return string|null The path to the console directory, null if not found.
+     */
+    protected static function getConsoleDir(CommandEvent $event, $actionName)
+    {
+        $options = static::getOptions($event);
+
+        if (static::useNewDirectoryStructure($options)) {
+            if (!static::hasDirectory($event, 'symfony-bin-dir', $options['symfony-bin-dir'], $actionName)) {
+                return;
+            }
+
+            return $options['symfony-bin-dir'];
+        }
+
+        if (!static::hasDirectory($event, 'symfony-app-dir', $options['symfony-app-dir'], 'execute command')) {
+            return;
+        }
+
+        return $options['symfony-app-dir'];
+    }
+
+    /**
+     * Returns true if the new directory structure is used.
+     *
+     * @param array $options Composer options
+     *
+     * @return bool
+     */
+    protected static function useNewDirectoryStructure(array $options)
+    {
+        return isset($options['symfony-var-dir']) && is_dir($options['symfony-var-dir']);
+    }
+
+    protected static function getOptions(CommandEvent $event)
+    {
+        $options = array_merge(static::$options, $event->getComposer()->getPackage()->getExtra());
+
+        $options['symfony-assets-install'] = getenv('SYMFONY_ASSETS_INSTALL') ?: $options['symfony-assets-install'];
+
+        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
+
+        return $options;
+    }
+
+    protected static function hasDirectory(CommandEvent $event, $configName, $path, $actionName)
+    {
+        if (!is_dir($path)) {
+            $event->getIO()->write(sprintf('The %s (%s) specified in composer.json was not found in %s, can not %s.', $configName, $path, getcwd(), $actionName));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    protected static function executeCommand(CommandEvent $event, $consoleDir, $cmd, $timeout = 300)
+    {
+        $php = escapeshellarg(static::getPhp(false));
+        $phpArgs = implode(' ', array_map('escapeshellarg', static::getPhpArguments()));
+        $console = escapeshellarg($consoleDir.'/console');
+        if ($event->getIO()->isDecorated()) {
+            $console .= ' --ansi';
+        }
+
+        $process = new Process($php.($phpArgs ? ' '.$phpArgs : '').' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) use ($event) { $event->getIO()->write($buffer, false); });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+        }
+    }
+
+    protected static function getPhp($includeArgs = true)
+    {
+        $phpFinder = new PhpExecutableFinder();
+        if (!$phpPath = $phpFinder->find($includeArgs)) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
+    }
+}
