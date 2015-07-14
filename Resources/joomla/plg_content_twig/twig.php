502969078e00918022c8accce8b8c28abb2c3bd2
Maximilian Ruta <mr@xtain.net>
2015-07-14 19:01:52 +0200

    add twig content plugin

---
+++ Resources/joomla/plg_content_twig/twig.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Content.Twig
+ *
+ * @copyright   Copyright (C) 2015 XTAIN oHG. All rights reserved.
+ * @license     see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+class_alias(XTAIN\Bundle\JoomlaBundle\Component\Plugin\Content\Twig::class, 'PlgContentTwig');
