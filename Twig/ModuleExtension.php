5e28152d3d9e9c9f6aab46984989aa1732e73537
Maximilian Ruta <mr@xtain.net>
2015-11-15 15:05:08 +0100

    update module extension

--- Twig/ModuleExtension.php
+++ Twig/ModuleExtension.php
@@ -22,6 +22,11 @@ use XTAIN\Bundle\JoomlaBundle\Joomla\JoomlaInterface;
 class ModuleExtension extends \Twig_Extension implements JoomlaAwareInterface
 {
     /**
+     * @var string
+     */
+    const TITLE_SEPARATOR = ' - ';
+
+    /**
      * @var JoomlaInterface
      */
     protected $joomla;
@@ -44,7 +49,9 @@ class ModuleExtension extends \Twig_Extension implements JoomlaAwareInterface
             new \Twig_SimpleFunction('joomla_component', [$this, 'component'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('joomla_head', [$this, 'head'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('joomla_module_position', [$this, 'renderModulePosition'], ['needs_environment' => true, 'is_safe' => ['html']]),
-            new \Twig_SimpleFunction('joomla_trans', [$this, 'trans'])
+            new \Twig_SimpleFunction('joomla_trans', [$this, 'trans']),
+            new \Twig_SimpleFunction('joomla_pagetitle', [$this, 'pagetitle']),
+            new \Twig_SimpleFunction('joomla_pageclass', [$this, 'pageclass'])
         ];
     }
 
@@ -144,6 +151,80 @@ class ModuleExtension extends \Twig_Extension implements JoomlaAwareInterface
         }
 
         return $text;
+    }
+
+    /**
+     * @return array
+     * @author Maximilian Ruta <mr@xtain.net>
+     */
+    public function pagetitle()
+    {
+        $app = \JFactory::getApplication();
+        $document = \JFactory::getDocument();
+        $documentTitle = $document->getTitle();
+
+        if ($app->getCfg('sitename_pagetitles', 0) == 1) {
+            $pagetTitleWithoutSitename = preg_quote(\JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), ''), '/');
+
+            if (preg_match('/^' . $pagetTitleWithoutSitename, $documentTitle)) {
+                $documentTitle = preg_replace('/^' . $pagetTitleWithoutSitename, '', $documentTitle);
+            }
+        } elseif ($app->getCfg('sitename_pagetitles', 0) == 2) {
+            $pagetTitleWithoutSitename = preg_quote(\JText::sprintf('JPAGETITLE', '', $app->getCfg('sitename')), '/');
+
+            if (preg_match('/' . $pagetTitleWithoutSitename . '$/', $documentTitle)) {
+                $documentTitle = preg_replace('/' . $pagetTitleWithoutSitename . '$/', '', $documentTitle);
+            }
+        }
+
+        $subtitle = '';
+
+        $itemid = \JRequest::getVar('Itemid');
+        $menu = $app->getMenu();
+        $active = $menu->getItem($itemid);
+        $completeTitle = null;
+        if (is_object($active)) {
+            $completeTitle = html_entity_decode($active->title);
+            $subtitle = $active->params->get('menu-anchor_title');
+        }
+
+        if (empty($completeTitle)) {
+            $completeTitle = $documentTitle;
+        }
+
+        $title = $completeTitle;
+
+        if (empty($subtitle)) {
+            $pos = strpos($completeTitle, self::TITLE_SEPARATOR);
+            if ($pos !== false) {
+                $title = substr($completeTitle, 0, $pos);
+                $subtitle = substr($completeTitle, 0, $pos + strlen(self::TITLE_SEPARATOR));
+            }
+        }
+
+        return [
+            'title'    => $title,
+            'subtitle' => $subtitle
+        ];
+    }
+
+    /**
+     * @return array
+     * @author Maximilian Ruta <mr@xtain.net>
+     */
+    public function pageclass()
+    {
+        $itemid = \JRequest::getVar('Itemid');
+        $menu = \JFactory::getApplication()->getMenu();
+        $active = $menu->getItem($itemid);
+
+        $pageclass = '';
+        if (is_object($active)) {
+            $params = $menu->getParams($active->id);
+            $pageclass = $params->get('pageclass_sfx');
+        }
+
+        return $pageclass;
     }
 
     public function getName()
